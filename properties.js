34cd75d5e6cb197aa398a07735a8b082bfeccb36
Wederson Cabral Domingues <wederson.domingues@stz.com.br>
2023-07-17 14:53:49 -0300

    Correção properties items

--- properties.js
+++ properties.js
@@ -4,32 +4,35 @@ define([], function () {
     return {
         type: "items",
         component: "accordion",
-        Configuration: {
-            type: "items",
-            label: "Configuration",
-            items: {
-                LabelDistance: {
-                    ref: "settings.object.id",
-                    label: "Object ID",
-                    type: "string",
-                    defaultValue: '',
-                    expression: "optional"
-                },
-                Width: {
-                    ref: "settings.width",
-                    label: "Width",
-                    type: "string",
-                    defaultValue: '1920',
-                    expression: "optional"
-                },
-                Height: {
-                    ref: "settings.height",
-                    label: "Height",
-                    type: "string",
-                    defaultValue: '1080',
-                    expression: "optional"
+        items: {
+            Configuration: {
+                type: "items",
+                label: "Configuration",
+                items: {
+                    LabelDistance: {
+                        ref: "settings.object.id",
+                        label: "Object ID",
+                        type: "string",
+                        defaultValue: '',
+                        expression: "optional"
+                    },
+                    Width: {
+                        ref: "settings.width",
+                        label: "Width",
+                        type: "string",
+                        defaultValue: '1920',
+                        expression: "optional"
+                    },
+                    Height: {
+                        ref: "settings.height",
+                        label: "Height",
+                        type: "string",
+                        defaultValue: '1080',
+                        expression: "optional"
+                    }
                 }
             }
         }
+
     }
 })
