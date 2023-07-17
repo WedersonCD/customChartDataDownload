586aae84d11427bed89a6c551a31bd9f20822da6
Wederson Cabral Domingues <wederson.domingues@stz.com.br>
2023-07-17 14:35:36 -0300

    Criaçaõ do setup inicial do javascript

---
+++ customChartDataDownload.js
@@ -0,0 +1,16 @@
+define([
+    'jquery',
+    './properties',
+    'qlik'
+    
+],function ($, props, qlik) {
+    'use strict'
+        return {
+            definition: props,
+            paint: function($element, layout){
+
+            }
+        }
+
+
+})
