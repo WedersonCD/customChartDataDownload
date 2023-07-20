91b5d8188f47da8e6fcafe7d376cc423e3a05a92
Wederson Cabral Domingues <wederson.domingues@stz.com.br>
2023-07-20 08:41:29 -0300

    Finalização V1 da extensão

--- customChartDataDownload.js
+++ customChartDataDownload.js
@@ -3,19 +3,66 @@ define([
     './properties',
     'qlik'
     
-],function ($, props, qlik) {
-    'use strict'
+],function ($, props,qlik) {
+        
+        function getPDFExportConfiguration(layout){
+
+            return JSON.parse(layout.settings.object.configuration)
+        }
+
+        function getBaseHTML(objectDivID, buttonId,iconClass,label) {
+            
+            //*
+            var html = `
+                <div style='height:100%;display: flex; flex-direction: column'>
+                    <div style='flex:2'><button class='lui-button' id=${buttonId} type='button'> <span class='${iconClass}'></span> ${label} </button></div>
+                    <div style='flex:8' id= '${objectDivID}'></div>
+                </div>
+            `;
+            //*/
+
+            return html
+    
+        }
+
+
+        function setExportButtonAction(BUTTOM_ID, object,layout) {
+            
+            if(layout.settings.type=='image'){
+
+                document.getElementById(BUTTOM_ID).onclick = function () {
+                    object.exportImg({height:layout.settings.image.height,width: layout.settings.image.width,format:'png'}).then((link) => {
+                        window.open(link)
+                    })
+                }
+            }else{
+                document.getElementById(BUTTOM_ID).onclick = function () {
+                    object.exportData({format: 'OOXML'}).then((link) => {
+                        window.open(link)
+                    })
+                } 
+            }
+
+
+        }
+
         return {
             definition: props,
-            paint: function($element, layout){
-                const mainDiv=document.createElement('div')
-                mainDiv.id='minha-tabela';
-                mainDiv.style.width='1920px';
-                mainDiv.style.height='1080px';
+            paint: async function($element, layout){
 
-                $element.append(mainDiv)
+                const MAIN_DIV_ID='Main_Div_'+layout.qInfo.qId
+                const BUTTOM_ID='Button_'+layout.qInfo.qId
 
-                
+                const baseHTML=getBaseHTML(MAIN_DIV_ID,BUTTOM_ID,layout.settings.html.buttomClass,layout.settings.html.buttomLabel)
+                $element.html(baseHTML)
+
+                const APP = await qlik.currApp(this);
+                const OBJ = await APP.visualization.get(layout.settings.object.id);
+
+                console.log('obeto: ',OBJ)
+
+                setExportButtonAction(BUTTOM_ID, OBJ, layout)
+
             }
         }
 
--- properties.js
+++ properties.js
@@ -4,33 +4,63 @@ define([], function () {
     return {
         type: "items",
         component: "accordion",
+
         items: {
             Configuration: {
                 type: "items",
                 label: "Configuration",
                 items: {
-                    LabelDistance: {
+                    ObjectId: {
                         ref: "settings.object.id",
                         label: "Object ID",
                         type: "string",
-                        defaultValue: '',
+                        defaultValue: 'UDHBZS',
                         expression: "optional"
                     },
+
+                    ButtomClass: {
+                        ref: "settings.html.buttomClass",
+                        label: "button class",
+                        type: "string",
+                        defaultValue: 'lui-icon lui-icon--download',
+                        expression: "optional"
+                    },
+                    ButtomLabel: {
+                        ref: "settings.html.buttomLabel",
+                        label: "Buttom Label",
+                        type: "string",
+                        defaultValue: 'Download',
+                        expression: "optional"
+                    },
+                    Type: {
+                        ref: "settings.type",
+                        label: "Data Label Position",
+                        type: "string",
+                        component: "dropdown",
+                        defaultValue: 'image',
+                        options: [
+                            { value: "image", label: "PNG" },
+                            { value: "XLSX", label: "XLSX" }
+                        ]
+                    },
                     Width: {
-                        ref: "settings.width",
+                        ref: "settings.image.width",
                         label: "Width",
                         type: "string",
                         defaultValue: '1920',
                         expression: "optional"
                     },
                     Height: {
-                        ref: "settings.height",
+                        ref: "settings.image.height",
                         label: "Height",
                         type: "string",
                         defaultValue: '1080',
                         expression: "optional"
-                    }
+                    },
                 }
+            },
+            appearance: {
+                uses: "settings",
             }
         }
 
